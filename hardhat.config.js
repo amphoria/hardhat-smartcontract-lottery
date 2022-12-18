f5de048b206dbb62c95d3c13be20f632a18f69d1
David <david@MARS>
2022-12-18 13:46:50 +0000

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,75 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            // // If you want to do some forking, uncomment this
+            // forking: {
+            //   url: MAINNET_RPC_URL
+            // }
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+        },
+    },
+    solidity: "0.8.7",
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Raffle"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 500000, // 500 secs max for running tests
+    },
+};
